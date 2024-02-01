e4a384d6e2fb2744c0260330bac3ffff34e9fdd2
Adrien.frances <133261374+adrinox@users.noreply.github.com>
2024-02-01 18:56:14 +0100

    Update scriptd2.sh

--- scriptd2.sh
+++ scriptd2.sh
@@ -24,4 +24,4 @@ set title " "
 set terminal png 
 set output 'graphique-d2.png'
 plot "CyTruck-MI3grpC/temp/resultatd2.txt" using 2:xtic(1) axes x1y2 lc rgb "skyblue" notitle
-
+mv graphique-d2.png CyTruck-MI3grpC/temp/
