c04759c06d9d30532231764ccd15f4f0341725d1
Adrien.frances <133261374+adrinox@users.noreply.github.com>
2024-02-02 04:48:52 +0100

    Update scriptt.sh

--- scriptt.sh
+++ scriptt.sh
@@ -5,7 +5,7 @@ set style fill solid
 set datafile separator ";"
 
 set boxwidth 0.4
-set xrange[0.5:*]
+set xrange[-0.5:*]
 set yrange [0:*]
 set style histogram gap 5
 set xtic rotate by -45
@@ -15,8 +15,8 @@ set terminal png size 800,1000
 
 set xlabel "Villes"
 set ylabel "Nb Routes"
-set title "Option -l"
+set title "Option -t"
 
 set terminal png 
-set output 'graphiquet.png'
-plot "-t.txt" using ($0):2 with boxes lc rgb "#32D365",'' using ($0+0.2):3 with boxes lc rgb "#3D9E5C" notitle
+set output 'grapht.png'
+plot "resultatt.txt" using ($0):2:xtic(1) with boxes lc rgb "#32D365" title "Nombre de trajets qui parcourent la ville", '' using ($0+0.2):3 with boxes lc rgb "#3D9E5C" title "Nombre de fois ville de départ"
